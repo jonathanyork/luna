34ce1093db72b04fa37abeb8060cf0fb5f848462
Jonathan York <jonathan.york@gmail.com>
2017-09-19 08:06:41 -0400

    initial commit

--- src/main/scala/com/bigdlittled/luna/Main.scala
+++ src/main/scala/com/bigdlittled/luna/Main.scala
@@ -8,48 +8,42 @@ import scalax.collection.edge.Implicits._ // shortcuts
 import scala.collection.mutable.ArrayBuffer
 import domain._
 
-trait D { def name: String }
-case class A(name: String)
-case class B(name: String)
-case class C(name: String) extends D
+case class Investment(name: String)
+case class User(name: String)
 
-trait ED
-
-case class EA[+N](fromNode: N, toNode: N)
+case class Holding[+N](fromNode: N, toNode: N)
   extends DiEdge[N](NodeProduct(fromNode, toNode))
   with    ExtendedKey[N]
-  with    EdgeCopy[EA]
-  with    OuterEdge[N,EA] 
-  with    ED
+  with    EdgeCopy[Holding]
+  with    OuterEdge[N,Holding] 
 {
   private def this(nodes: Product) {
     this(nodes.productElement(0).asInstanceOf[N],
          nodes.productElement(1).asInstanceOf[N])
   }
   def keyAttributes = Seq()
-  override def copy[NN](newNodes: Product) = new EA[NN](newNodes)
-  override protected def attributesToString = s"EA" 
+  override def copy[NN](newNodes: Product) = new Holding[NN](newNodes)
+  override protected def attributesToString = s"Holding" 
 }
 
-case class EB[+N](fromNode: N, toNode: N)
+case class Permission[+N](fromNode: N, toNode: N, something: Int)
   extends DiEdge[N](NodeProduct(fromNode, toNode))
   with    ExtendedKey[N]
-  with    EdgeCopy[EB]
-  with    OuterEdge[N,EB]
-  with    ED
+  with    EdgeCopy[Permission]
+  with    OuterEdge[N,Permission]
 {
-  private def this(nodes: Product) {
+  private def this(nodes: Product, something: Int) {
     this(nodes.productElement(0).asInstanceOf[N],
-         nodes.productElement(1).asInstanceOf[N])
+         nodes.productElement(1).asInstanceOf[N], something)
   }
   def keyAttributes = Seq()
-  override def copy[NN](newNodes: Product) = new EB[NN](newNodes)
-  override protected def attributesToString = s"EB" 
+  override def copy[NN](newNodes: Product) = new Permission[NN](newNodes, something)
+  override protected def attributesToString = s"Permission" 
 }
 
 object ED {
-  implicit final class ImplicitEdge[A <: D](val e: DiEdge[A]) extends AnyVal {
-    def ## (something: String) = new EA[A](e.source, e.target)
+  implicit final class ImplicitEdge[Investment](val e: DiEdge[Investment]) extends AnyVal {
+    def ## (something: String) = new Holding[Investment](e.source, e.target)
   } 
 }
 
@@ -57,11 +51,11 @@ object Main extends App {
   //import ED._
 
   val g = Graph(
-    A("One")~>A("Two"),
-      EA(A("Two"),A("Three")),
-    EB(A("One"),C("Four")),
-      EA(A("Four"),B("Five")),
-    (A("One")~+>A("Six"))(0.5)
+    Investment("One")~>Investment("Two"),
+      Holding(Investment("Two"),Investment("Three")),
+    Permission(Investment("One"),User("Four"),2),
+      Holding(Investment("Four"),User("Five")),
+    (Investment("One")~+>Investment("Six"))(0.5)
   )
 
   // All the nodes
@@ -70,13 +64,15 @@ object Main extends App {
   // All the holdings
   println(g.edges mkString ":")
 
-  val g2 = g + (A("One")~+>A("Six"))(0.2)
+  val g2 = g + (Investment("One")~+>Investment("Six"))(0.2)
   // All the nodes
   println(g2.edges mkString ":")
   
   // The top level portfolio
-  val p = g get A("One")
- 
+  val p = g get Investment("One")
+
+  val q = g find Investment("One")
+  
   // All the nodes with a traverser 
   println(p.outerNodeTraverser.map(_.getClass.toString()))
 
@@ -89,12 +85,10 @@ object Main extends App {
   // All the nodes with a traverser 
   println(p.innerEdgeTraverser.map(_.getClass.toString()))
 
-/*
   // Same thing with a for comprehension
   println(
     for {
       a <- p.outerNodeTraverser
     } yield a.toString()
   )
-*/
 }
